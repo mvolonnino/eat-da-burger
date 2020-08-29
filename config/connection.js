4276413a020aa53bd65ea7355e18fc2298eddb1b
Matt Volonnino <mvolonnino12@gmail.com>
2020-08-29 12:32:56 -0400

    added JAWSDB to connection.js to work with heruko

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // connect to mySQL
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQL_KEY,
-  database: "burgers_db",
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_KEY,
+    database: "burgers_db",
+  });
+}
 
 // make the connection
 connection.connect(function (err) {
